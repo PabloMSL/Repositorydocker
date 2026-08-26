1010705f72d5d4bac598e2c79dcd9de127ef6ffe
PabloMSL <jpablomilitar@gmail.com>
2026-08-25 22:35:24 -0500

    Fallo intencional

--- sample_app.py
+++ sample_app.py
@@ -7,7 +7,7 @@ sample = Flask(__name__)
 @sample.route("/")
 def home():
     try:
-        conn = pymysql.connect(host='servidor-bd', user='root', password=os.getenv("MYSQL_ROOT_PASSWORD"), database='082_db') # nosec B106
+        conn = pymysql.connect(host='servidor-bd', user='root', password="1234", database='082_db') # nosec B106
         conn.close()
         db_status = "Conexión exitosa a la base de datos"
     except Exception as e:
@@ -15,3 +15,6 @@ def home():
 
     return f"<h1>Bienvenido a mi aplicación Flask</h1><p>{db_status}</p>"
 
+if __name__ == "__main__":
+    modo_debug = os.getenv("FLASK_DEBUG", "True").lower() == "true"
+    sample.run(host='0.0.0.0', port=5050, debug=modo_debug) # nosec B104
