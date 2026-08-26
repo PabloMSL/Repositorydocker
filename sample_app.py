aefec1826d6e10484603d97aea5e04e99296a5f9
PabloMSL <jpablomilitar@gmail.com>
2026-08-25 22:21:01 -0500

    Arreglo del servidor-bd

--- sample_app.py
+++ sample_app.py
@@ -1,13 +1,12 @@
 import pymysql
 from flask import Flask
-import os
 
 sample = Flask(__name__)
 
 @sample.route("/")
 def home():
     try:
-        conn = pymysql.connect(host='servidor-bd-ejemplo', user='root', password='os.getenv("MYSQL_ROOT_PASSWORD")', database='082_db') # nosec B106
+        conn = pymysql.connect(host='servidor-bd', user='root', password='os.getenv("MYSQL_ROOT_PASSWORD")', database='082_db') # nosec B106
         conn.close()
         db_status = "Conexión exitosa a la base de datos"
     except Exception as e:
